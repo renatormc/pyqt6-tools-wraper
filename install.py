876ae5e2bf4a3cf5b52648ebecdcb53e96cdc5b9
unknown <renatomartinsrmc@gmail.com>
2023-05-22 08:55:41 -0300

    normal

--- install.py
+++ install.py
@@ -1,14 +1,26 @@
 from pathlib import Path
 import os
+import subprocess
 
 app_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
-python = app_dir / ".venv\\Scripts\\python.exe"
+def get_poetry_venv():
+    text = subprocess.getoutput("poetry env info")
+    lines = text.split("\n")
+    for line in lines:
+        if line.startswith("Path: "):
+            return Path(line.replace("Path: ", "").strip())
+    raise Exception("Executable not found")
+
+python_folder = get_poetry_venv()
+activate = python_folder / "Scripts/activate.bat"
+
 p = app_dir / "main.py"
 folder = input("Destination folder: ")
 p2 = Path(folder) / "p6w.bat"
 
 text = f"""@echo off
-\"{python}\" \"{p}\" %*
+"{activate}"
+python \"{p}\" %*
 """
 p2.write_text(text)
