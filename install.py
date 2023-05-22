6b499868ba528563ce6b2e4f76774666ac3727b6
renatormc <renatomartinsrmc@gmail.com>
2023-05-22 12:35:26 -0300

    install on linux

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import subprocess
+import stat
 
 app_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
@@ -13,14 +14,26 @@ def get_poetry_venv():
     raise Exception("Executable not found")
 
 python_folder = get_poetry_venv()
-activate = python_folder / "Scripts/activate.bat"
+activate = python_folder / "Scripts/activate.bat" 
 
 p = app_dir / "main.py"
 folder = input("Destination folder: ")
-p2 = Path(folder) / "p6w.bat"
 
-text = f"""@echo off
-"{activate}"
-python \"{p}\" %*
-"""
-p2.write_text(text)
+if os.name == "windows":
+    activate = python_folder / "Scripts/activate.bat"
+    p2 = Path(folder) / "p6w.bat"
+    text = f"""@echo off
+    "{activate}"
+    python \"{p}\" %*
+    """
+    p2.write_text(text)
+else:
+    activate = python_folder / "bin/activate"
+    p2 = Path(folder) / "p6w"
+    text = f"""#!/bin/bash
+    source \"{activate}\"
+    python \"{p}\" $@
+    """
+    p2.write_text(text)
+    st = os.stat(p2)
+    os.chmod(p2, st.st_mode | stat.S_IEXEC)
